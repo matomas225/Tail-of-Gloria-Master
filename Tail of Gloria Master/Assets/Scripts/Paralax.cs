9b7a40afb4140125959bb3a57772a8eed2b8656b
Matomas <matomas225@users.noreply.github.com>
2020-01-23 21:40:19 +0200

    0.0.1a
    + background
    + parallax

---
+++ Tail of Gloria Master/Assets/Scripts/Paralax.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paralax : MonoBehaviour
+{
+    private float lenght, startpos;
+    public GameObject cam;
+    public float paralax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startpos = transform.position.x;
+        lenght = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float temp = (cam.transform.position.x * (1 - paralax));
+        float dist = (cam.transform.position.x * paralax);
+
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+        if (temp > startpos + lenght) startpos += lenght;
+        else if (temp < startpos - lenght) startpos -= lenght;
+    }
+}
